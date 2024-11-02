e005d44be5ba3e80e82794e245a07844a98a202c
Tgithinji <t.muraguri420@gmail.com>
2024-11-02 20:23:25 +0300

    Enable CORS(cross origin resource sharing)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from .database import init_db
 from .routes import blog, user, auth, comments
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ['*']
+
+
+# CORS function that runs before every request
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # initialize database tables
 init_db()
